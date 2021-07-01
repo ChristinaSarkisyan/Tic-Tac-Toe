437cd689a8c159bcbbbb0a21bf85d2dfd1b6ef91
ChristinaSarkisyan <58526236+ChristinaSarkisyan@users.noreply.github.com>
2021-07-01 13:24:31 -0700

    framework for driver and board object added

---
+++ board.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  1 16:06:54 2021
+
+@author: chris
+"""
+
+class board :
+    def __init__():
+        print("test")
+        
+    def update_board ():
+        print("test")
+        #reads new input from user and updates board object to match
+        
+    def return_board():
+        print("test")
+        
+        
+    def show_board():
+        print("test")
+        
+    def check_if_won():
+        print("test")
+        #if player has 3 in a row, display "you won"
+        #if cpu has 3 in a row, display "you lost
+        #(if make multiplayer) if player 2 won, display "(name) won" and vice versa
+   
+    def check_if_tie():
+        print("test")
+        #if all spaces taken, display "tie" message
---
+++ driver.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  1 16:14:58 2021
+
+@author: chris
+"""
+
+import board.py
+
+#make a board
+#display
+#have user interact with GUI
+#update board
+#display board
+
+#repeat until someone has 3 in a row
+
+#show congradulation message
