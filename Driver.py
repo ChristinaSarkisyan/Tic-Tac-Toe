999a35989c61b6faf10c560d4021b438509122e9
ChristinaSarkisyan <58526236+ChristinaSarkisyan@users.noreply.github.com>
2021-07-02 14:39:31 -0700

    changed how buttons are stored

--- Driver.py
+++ Driver.py
@@ -11,27 +11,31 @@ import Analysis as ans
 import numpy as np
 import Board_Minder as bm
 
-def on_click():
-    print("you clicked a button")
+def on_click(code):
+    print("you clicked a button ", code)
+    #self.state =DISABLED
 
 msg.Message()
 #bd.Board()
 buttons = []
 root = Tk()
 root.title('Tik Tac Toe')
+
 #make all the buttons
-for i in range(3):
-    for j in range(3):
-        buttons.append(Button(root, text ="", padx=50, pady=50, command= on_click )) 
+b1a = Button(root, text ="b1a", padx=50, pady=50, command=lambda: on_click("b1a")).grid(row=1, column=0)
+b1b = Button(root, text ="b1b", padx=50, pady=50, command=lambda: on_click("b1b")).grid(row=1, column=1)
+b1c = Button(root, text ="b1c", padx=50, pady=50, command=lambda: on_click("b1c")).grid(row=1, column=2)
+b2a = Button(root, text ="b2a", padx=50, pady=50, command=lambda: on_click("b2a")).grid(row=2, column=0)
+b2b = Button(root, text ="b2b", padx=50, pady=50, command=lambda: on_click("b2b")).grid(row=2, column=1)
+b2c = Button(root, text ="b2c", padx=50, pady=50, command=lambda: on_click("b2c")).grid(row=2, column=2)
+b3a = Button(root, text ="b3a", padx=50, pady=50, command=lambda: on_click("b3a")).grid(row=3, column=0)
+b3b = Button(root, text ="b3b", padx=50, pady=50, command=lambda: on_click("b3b")).grid(row=3, column=1)
+b3c = Button(root, text ="b3c", padx=50, pady=50, command=lambda: on_click("b3c")).grid(row=3, column=2)
 
 #make title
-title = Label(root, text = "Place your X", font =("arial", 15) , justify="center")
+title = Label(root, text = "Place your X", font =("arial", 15) , justify="center").grid(row=0, column=1)
    
-#put them into the window
-for i in range(3):
-    for j in range(3):
-        buttons[i + (j*3) - 1].grid(row=i+1, column=j)
-title.grid(row=0, column=1)
+
 root.mainloop()
 
 
