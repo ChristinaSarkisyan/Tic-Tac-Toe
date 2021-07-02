8ff8bcef0953e03c318ab8342695497ea4e853e3
ChristinaSarkisyan <58526236+ChristinaSarkisyan@users.noreply.github.com>
2021-07-02 12:15:32 -0700

    changed and updated ho board is generated

--- Board.py
+++ Board.py
@@ -5,35 +5,62 @@ Created on Thu Jul  1 16:06:54 2021
 @author: chris
 """
 
-import tkinter as tk
+from tkinter import *
 import Analysis as ans
+import numpy as np
 
-class Board(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
+
+class Board():
+    def __init__(self):
+        self.buttons = []
+        root = Tk()
+        root.title('Tik Tac Toe')
+        #make all the buttons
+        for i in range(3):
+            for j in range(3):
+                self.buttons.append(Button(root, text ="", padx=50, pady=50)) #add padding
         
-        brain = ans.Analysis('bl')
+        #make title
+        self.title = Label(root, text = "Place your X", font =("arial", 15) , justify="center")
+       
+        #put them into the window
+        for i in range(3):
+            for j in range(3):
+                self.buttons[i + (j*3) - 1].grid(row=i+1, column=j)
+        self.title.grid(row=0, column=1)
+        root.mainloop()
         
-    def create_widgets(self):
-        self.greeting = tk.Button(self)
-        self.greeting["text"] = "welcome to boarddd"
-        self.greeting.pack(side="top")
+        
+        
+        #brain = ans.Analysis('bl')
+        
+    # def create_widgets(self):
+    #     for i in range(3):
+    #         for j in range(3):
+    #             self.buttons[i][j] = tk.Button(self)#, text ="testing", padx=50, pady=50)
+        
+    #     print("buttons after create widget\n", self.buttons, "\n\n") #temp testor
+    #     # self.buttonsButton(self)
+    #     # self.greeting["text"] = "welcome to boarddd"
+    #     #self.greeting.pack(side="top")
+        
+    #     for i in self.buttons:
+    #          i.pack()
     
         
         
         
-    def update_board ():
-        print("test")
-        #reads new input from user and updates board object to match
+    # def update_board ():
+    #     print("test")
+    #     #reads new input from user and updates board object to match
         
-    def return_board():
-        print("test")
+    # def return_board():
+    #     print("test")
         
         
-    def show_board():
-        print("test")
+    # def show_board():
+    #     print("test")
         
-    
+        
+    # def disable_buttons():
+    #     print("buttons disabled")
--- Testor.py
+++ Testor.py
@@ -6,24 +6,37 @@ Created on Thu Jul  1 16:58:55 2021
 """
 
 #import Board
-import Message
-import tkinter as tk
+# import Message
+# import tkinter as tk
+# import Board as bd
+
+# #get message running
+# root = tk.Tk()
+# root.geometry("300x200")
+# root.maxsize(300, 200)
+# root.title('Tik Tac Toe')
+# app = Message.Message(master=root)
+
+
+# app.mainloop()
+
+
+# root1 = tk.Tk()
+# root1.geometry("600x600")
+# root1.maxsize(600, 600)
+# root1.title('Tik Tac Toe')
+# game = bd.Board(master=root1)
+# game.mainloop()
+
+# from tkinter import *
+
+# root = Tk()
+# b1 = Button(root, text = "hi")
+# b2 = Button(root, text = "hi")
+
+# b1.grid(row=0, column=0)
+# b2.grid(row=1, column=0)
+# root.mainloop()
+
 import Board as bd
-
-#get message running
-root = tk.Tk()
-root.geometry("300x200")
-root.maxsize(300, 200)
-root.title('Tik Tac Toe')
-app = Message.Message(master=root)
-
-
-app.mainloop()
-
-
-root1 = tk.Tk()
-root1.geometry("600x600")
-root1.maxsize(600, 600)
-root1.title('Tik Tac Toe')
-game = bd.Board(master=root1)
-game.mainloop()
+game = bd.Board()
